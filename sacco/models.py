9d4e42d131744fec252a82bfb6da6fdea0cfe925
Larry <lawrencengugi90@gmail.com>
2024-11-13 12:09:52 +0300

    Full Commit

---
+++ sacco/models.py
@@ -0,0 +1,16 @@
+from django.core.management.commands import makemigrations
+from django.db import models
+
+# Create your models here.
+class Customer(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    dob = models.DateField()
+    gender = models.CharField(max_length=10)
+    weight = models.IntegerField(default=0)
+
+
+# run the migration
+# python manage.py makemigrations
+# python manage.py migrate
